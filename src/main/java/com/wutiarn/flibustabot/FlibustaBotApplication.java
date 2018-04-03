90e7c07cd51481cac10a6d2403aff61904984b75
Dmitry Romanov <dev@wutiarn.com>
2018-04-03 08:34:49 +0300

    Init

---
+++ src/main/java/com/wutiarn/flibustabot/FlibustaBotApplication.java
@@ -0,0 +1,12 @@
+package com.wutiarn.flibustabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlibustaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlibustaBotApplication.class, args);
+	}
+}
