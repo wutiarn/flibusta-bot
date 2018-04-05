1d56e5781cf5e14b174752aaf9c454144f3bee34
Dmitry Romanov <dev@wutiarn.com>
2018-04-05 23:02:19 +0300

    WIP: Process telegram updates

--- src/main/java/com/wutiarn/flibustabot/service/TelegramBotService.java
+++ src/main/java/com/wutiarn/flibustabot/service/TelegramBotService.java
@@ -1,17 +1,14 @@
 package com.wutiarn.flibustabot.service;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.GetUpdates;
-import com.pengrad.telegrambot.response.GetUpdatesResponse;
+import com.pengrad.telegrambot.request.GetMe;
+import com.pengrad.telegrambot.response.GetMeResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class TelegramBotService implements ApplicationRunner {
@@ -24,27 +21,24 @@ public class TelegramBotService implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        new Thread(this::receiveUpdates).start();
-    }
+        testBotConfiguration();
 
-    private void receiveUpdates() {
-        AtomicInteger lastOffset = new AtomicInteger();
-        //noinspection InfiniteLoopStatement
-        while (true) {
-            GetUpdatesResponse response = bot.execute(new GetUpdates().timeout(60).offset(lastOffset.get()));
-            List<Update> updates = response.updates();
-            if (updates == null) {
-                continue;
-            }
-
-            updates.forEach((update) -> {
-                lastOffset.set(update.updateId() + 1);
+        bot.setUpdatesListener(updates -> {
+            updates.forEach(update -> {
                 Message message = update.message();
-
                 if (message != null) {
                     processMessage(message);
                 }
             });
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+    }
+
+    private void testBotConfiguration() {
+        GetMeResponse resp = bot.execute(new GetMe());
+        if (!resp.isOk()) {
+            throw new IllegalStateException(String.format("Can't use getMe telegram method. " +
+                    "Is bot token valid?. Error code: %d", resp.errorCode()));
         }
     }
 
