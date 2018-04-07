f656df4dc78104ace17e5c85561e54122470f0f9
Dmitry Romanov <dev@wutiarn.com>
2018-04-07 17:46:35 +0300

    WIP: Render search response template

--- src/main/java/com/wutiarn/flibustabot/service/TelegramEventsRouterService.java
+++ src/main/java/com/wutiarn/flibustabot/service/TelegramEventsRouterService.java
@@ -3,19 +3,33 @@ package com.wutiarn.flibustabot.service;
 import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.BaseResponse;
 import com.wutiarn.flibustabot.model.opds.BookSearchResult;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
 
 @Service
 public class TelegramEventsRouterService {
     Logger logger = LoggerFactory.getLogger(TelegramEventsRouterService.class);
 
     final FlibustaService flibustaService;
+
+    @Autowired
+    Configuration freemarkerConfiguration;
 
     @Autowired
     public TelegramEventsRouterService(FlibustaService flibustaService) {
@@ -29,13 +43,33 @@ public class TelegramEventsRouterService {
         }
 
         if (message.text() != null) {
-            handleSearchRequest(message);
+            return handleSearchRequest(message);
         }
         return null;
     }
 
-    private void handleSearchRequest(Message message) {
+    private BaseRequest<? extends BaseRequest, ? extends BaseResponse> handleSearchRequest(Message message) {
         String query = message.text();
         BookSearchResult searchResults = flibustaService.search(query, FlibustaService.SearchType.BOOKS);
+
+        var data = new HashMap<String, Object>();
+        data.put("query", "hello world");
+
+        String renderedResult;
+
+        try {
+            Template template = freemarkerConfiguration.getTemplate("searchResults.ftl");
+
+            ByteArrayOutputStream resultStream = new ByteArrayOutputStream();
+            OutputStreamWriter writer = new OutputStreamWriter(resultStream);
+            template.process(data, writer);
+
+            writer.close();
+            renderedResult = resultStream.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return new SendMessage(message.chat().id(), renderedResult).parseMode(ParseMode.HTML);
     }
 }
---
+++ src/main/resources/templates/searchResults.ftl
@@ -0,0 +1,2 @@
+<#-- @ftlvariable name="query" type="java.lang.String" -->
+<b>Результаты поиска по запросу ${query}</b>
