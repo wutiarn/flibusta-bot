474afa7d72969331c3aba0342b94c275678afade
Dmitry Romanov <dev@wutiarn.com>
2018-04-08 13:35:17 +0300

    Log book download events

--- src/main/java/com/wutiarn/flibustabot/telegram/commands/DownloadCommandHandler.java
+++ src/main/java/com/wutiarn/flibustabot/telegram/commands/DownloadCommandHandler.java
@@ -8,6 +8,8 @@ import com.pengrad.telegrambot.response.SendResponse;
 import com.wutiarn.flibustabot.exceptions.flibusta.BookBlockedException;
 import com.wutiarn.flibustabot.model.opds.BookFile;
 import com.wutiarn.flibustabot.service.FlibustaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +22,8 @@ import java.util.regex.Pattern;
 @Component
 public class DownloadCommandHandler implements TelegramCommandHandler {
     private static final Pattern messagePattern = Pattern.compile("/dl_(?<format>\\w+)_(?<id>\\d+)");
+
+    private final Logger logger = LoggerFactory.getLogger(DownloadCommandHandler.class);
 
     private final FlibustaService flibustaService;
     private final TelegramBot bot;
@@ -56,12 +60,15 @@ public class DownloadCommandHandler implements TelegramCommandHandler {
     private void downloadAndSendBook(long chatId, int requestMessageId, int statusMessageId, String bookId, String bookFormat) {
         bot.execute(new EditMessageText(chatId, statusMessageId, "Загрузка"));
         BookFile bookFile;
+        logger.info(String.format("Downloading book %s (%s)", bookId, bookFormat));
         try {
             bookFile = flibustaService.getBookFile(bookId, bookFormat);
         } catch (IOException e) {
+            logger.warn(String.format("Exception occurred while downloading book %s (%s)", bookId, bookFormat), e);
             bot.execute(new EditMessageText(chatId, statusMessageId, String.format("Произошла ошибка: %s", e)));
             return;
         } catch (BookBlockedException e) {
+            logger.info(String.format("Download failed: book %s (%s) is blocked", bookId, bookFormat));
             bot.execute(new EditMessageText(chatId, statusMessageId, "К сожалению доступ к данной " +
                     "книге ограничен по требованию правоторговца"));
             return;
@@ -69,5 +76,7 @@ public class DownloadCommandHandler implements TelegramCommandHandler {
         bot.execute(new EditMessageText(chatId, statusMessageId, "Отправка"));
         bot.execute(new SendDocument(chatId, bookFile.content).fileName(bookFile.filename).replyToMessageId(requestMessageId));
         bot.execute(new DeleteMessage(chatId, statusMessageId));
+
+        logger.info(String.format("Book sent: %s (%s)", bookId, bookFormat));
     }
 }
